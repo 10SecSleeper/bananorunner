308d57ef26198b8e967c0437f9801133b931fa9d
mitchellcairns <mitchellcairns@gmail.com>
2018-03-12 21:01:26 -0700

    Infinite players allowed on server

---
+++ Assets/Memefy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class Memefy : MonoBehaviour {
+
+    AudioSource aud;
+
+	// Use this for initialization
+	void Start () {
+
+        aud = GetComponent<AudioSource>();
+
+        GameObject[] memes = GameObject.FindGameObjectsWithTag("Meme");
+
+        bool play = true;
+
+        foreach (GameObject joke in memes)
+        {
+            AudioSource audt = joke.GetComponent<AudioSource>();
+
+            if (audt.isPlaying == true && joke != this.gameObject)
+            {
+                play = false;
+            }
+
+        }
+
+        if (play)
+            aud.Play();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
